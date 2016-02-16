5e8efbd3c926b1d3339987b8e0b72fdd8ac366ca
Jacson Passold <jacson@ethymos.com.br>
2016-02-16 19:28:47 -0200

    first commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+class WpLoginCidadao
+{
+	public function __construct()
+	{
+		
+	}
+	
+}
+
+global $WpLoginCidadao;
+$WpLoginCidadao = new WpLoginCidadao();
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+header("Refresh: 0;url=".get_template_directory_uri()."/web/app_dev.php"); 
+
+?>
